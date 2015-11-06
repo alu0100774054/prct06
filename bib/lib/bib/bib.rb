0675b1fd8044447ccd69e75e455f6ddcc668483f
erikbv99421 <erikbv99421@gmail.com>
2015-11-06 11:30:06 +0000

    expectativa del titulo

--- bib/lib/bib/bib.rb
+++ bib/lib/bib/bib.rb
@@ -3,9 +3,10 @@ require "bib/version"
 module Bib
   # Your code goes here...
   class Libro
-  	attr_accessor :autor
-  	def initialize autor
+  	attr_accessor :autor, :titulo
+  	def initialize autor, titulo
   		@autor=autor
+      @titulo=titulo
  		end
   end
 end
--- bib/spec/libro_spec.rb
+++ bib/spec/libro_spec.rb
@@ -4,8 +4,8 @@ require 'lib'
 module Prct06
 	class Libro
 		describe Prct06::Libro do
-			before :each do
-				@Libro = Libro.new "Autor"
+			before :all do
+				@Libro = Libro.new "Autor", "titulo"
 			end
 			describe '#new' do
 				it "Introduce parametros y retorna un objeto tipo Libro" do
@@ -15,6 +15,11 @@ module Prct06
 			describe '#autor' do
 				it "Retorna el autor del libro" do
 					@Libro.autor.should eql "autor"
+				end
+			end
+			describe '#titulo' do
+				it "Retorna el titulo del libro" do
+					@Libro.titulo.should eql "titulo"
 				end
 			end
 		end
