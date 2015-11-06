b1e54b5e420b86e68a3f1707faa99395cd87b905
erikbv99421 <erikbv99421@gmail.com>
2015-11-06 12:44:19 +0000

    arreglando los errores fusionando

--- bib/lib/bib/bib.rb
+++ bib/lib/bib/bib.rb
@@ -3,7 +3,6 @@ require "bib/version"
 module Bib
   # Your code goes here...
   class Libro
-<<<<<<< HEAD
   	attr_accessor :autor, :titulo, :editorial, :edicion, :fecha, :isbn
   		def initialize(autor, titulo, editorial, edicion, fecha, isbn)
   			@autor = autor
@@ -16,16 +15,5 @@ module Bib
  		def to_s
  			"Libro - #{@autor}, #{@titulo}, #{@editorial}, #{@edicion}, #{@fecha}, #{@isbn}"
  		end
-=======
-  	attr_accesor :autor, :titulo, :editorial, :edicion, :fecha, :isbn
-  	def initialize(autor,titulo,editorial,edicion,fecha,isbn)
-  		@autor=autor
-  		@titulo=titulo
-  		@editorial=editorial
-  		@edicion=edicion
-  		@fecha=fecha
-  		@isbn=isbn
-  	end
->>>>>>> master
   end
 end
--- bib/lib/lib.rb
+++ bib/lib/lib.rb
@@ -1,6 +1,4 @@
 require 'bib/version'
-<<<<<<< HEAD
 require 'bib/bib'
-=======
-require 'bib/prct06'
->>>>>>> master
+
+
